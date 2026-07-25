140f5184f13ae4de7303e4d8b2cab98cb0bec4f0
Maidaseu <248063980+Maidaseu@users.noreply.github.com>
2026-07-25 14:17:45 +0100

    Set up application routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
 function App() {
-  return <h1>Hello, Portfolio!</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 }
 
 export default App;
