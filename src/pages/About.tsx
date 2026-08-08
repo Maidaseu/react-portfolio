1aed33ee5ba71b9c1582e1e093ca5bfedc029531
Maidaseu <248063980+Maidaseu@users.noreply.github.com>
2026-08-08 17:36:02 +0100

    Create reusable container component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,15 @@
 import Section from "../components/Section";
+import Container from "../components/Container";
 
 export default function About() {
   return (
     <Section title="About Me">
-      <p>
-        I'm a frontend developer currently studying React, TypeScript and modern
-        web development while building real-world projects.
-      </p>
+      <Container>
+        <p>
+          I'm a frontend developer currently studying React, TypeScript and
+          modern web development while building real-world projects.
+        </p>
+      </Container>
     </Section>
   );
 }
